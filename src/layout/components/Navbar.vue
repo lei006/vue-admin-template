086531c1bcbc5c8dd429a202a155e19f7b674f66
lei006 <lei006@qq.com>
2022-07-08 04:34:21 +0800

    修改搜索

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -4,7 +4,16 @@
     <breadcrumb class="breadcrumb-container" />
     <div class="navbar-search-box">
       
-      <el-input size="small" placeholder="请输入内容" suffix-icon="el-icon-search" v-model="inputSearchText"></el-input>
+      <el-input size="small" placeholder="请输入内容" v-model="inputSearchText" class="input-with-select">
+        <el-select v-model="select" slot="prepend" placeholder="请选择" style="width:100px">
+          <el-option label="餐厅名" value="1"></el-option>
+          <el-option label="订单号" value="2"></el-option>
+          <el-option label="用户电话" value="3"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+
+
 
     </div>
     <div class="right-menu not-allow-select">
@@ -56,6 +65,7 @@ export default {
   data() {
     return {
       storeLayout: {},
+      select:"1",
       inputSearchText:"1111111111111"
     }
   },
@@ -140,4 +150,13 @@ export default {
     margin: 3px 3px;
   }
 }
+
+
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+
 </style>
